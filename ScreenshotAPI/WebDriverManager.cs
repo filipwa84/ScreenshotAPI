fa91e15c26ccd5b7f32ab2475e8776e9c391db92
Filip Wallberg <filip.wallberg@gmail.com>
2025-02-04 12:15:58 +0700

    Removed cookie reset options

--- ScreenshotAPI/WebDriverManager.cs
+++ ScreenshotAPI/WebDriverManager.cs
@@ -44,8 +44,8 @@ namespace ScreenshotAPI
                     options.AddArgument($"--force-lang={language}");
                     options.AddArgument($"--accept-lang={language}");
                     options.AddUserProfilePreference($"intl.accept_languages", $"{language},{language[..2]};q=0.9");
-                    options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
-                    options.AddUserProfilePreference("profile.block_third_party_cookies", true);
+                    //options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
+                    //options.AddUserProfilePreference("profile.block_third_party_cookies", true);
                 }
 
                 var driverService = ChromeDriverService.CreateDefaultService();
