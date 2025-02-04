c37e08beacbad763894d6d9b0b822342ebae3027
Filip Wallberg <filip.wallberg@gmail.com>
2025-02-04 12:06:24 +0700

    Refactored return statement

--- ScreenshotAPI/WebDriverManager.cs
+++ ScreenshotAPI/WebDriverManager.cs
@@ -26,8 +26,8 @@ namespace ScreenshotAPI
             }
 
             var threadId = Thread.CurrentThread.ManagedThreadId;
-            
-            return _webDrivers.GetOrAdd(threadId, _ =>
+
+            var result = _webDrivers.GetOrAdd(threadId, _ =>
             {
                 var options = new ChromeOptions();
                 options.AddArgument("--headless=new");
@@ -43,7 +43,7 @@ namespace ScreenshotAPI
                     options.AddArgument($"--lang={language}");
                     options.AddArgument($"--force-lang={language}");
                     options.AddArgument($"--accept-lang={language}");
-                    options.AddUserProfilePreference($"intl.accept_languages", $"{language},{language[..2]};q=0.9");                   
+                    options.AddUserProfilePreference($"intl.accept_languages", $"{language},{language[..2]};q=0.9");
                     options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
                     options.AddUserProfilePreference("profile.block_third_party_cookies", true);
                 }
@@ -60,7 +60,9 @@ namespace ScreenshotAPI
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
 
                 return (driver, driverService);
-            }).Driver;
+            });
+
+            return result.Driver;
         }
 
         public void DisposeDriver()
